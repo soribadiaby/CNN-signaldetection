b2574daa93d5d27129a658eb57548b9358096d65
Soriba Diaby <soriba.diaby@engie.com>
2019-09-03 11:28:46 +0200

    process.py : added comments

--- process.py
+++ process.py
@@ -7,11 +7,11 @@ Created on Tue May 14 18:59:20 2019
 import os
 import numpy as np
 
-#max_y est le nombre de pixels
+#max_y is the number of pixels (y axis)
 path_5000='5000ms'
 
-classes = ['AIS','DMR','EDACS48','EDACS96','NXDN48','NXDN96','ProtocolA','ProtocolB',
- 'ProtocolC','ProtocolD']  #np.load('classes.npy')
+classes = ['AIS','DMR','EDACS48','EDACS96','NXDN48','NXDN96','ProtocolA',
+           'ProtocolB', 'ProtocolC','ProtocolD'] 
 
 burst_durations = [26.6,1800,60,30,168,84,100,100,'continu','continu']
 
@@ -26,30 +26,25 @@ def get_px(max_x,max_y):
             burst_durations_px.append(max_y)
         else:
             burst_durations_px.append(np.ceil(max_y*burst/5000))
-        
-        bandwidths_px.append(np.ceil(max_x*bandwidth/1e3))  #On a un span de 1e6 et une bande passante en kHz, donc en divisent il reste 1e3
+        #span = 1e6 and bandwith is in kHz, so by dividing, we have 1e3
+        bandwidths_px.append(np.ceil(max_x*bandwidth/1e3))  
     return bandwidths_px, burst_durations_px
         
-
-#caracterisiques = dict(zip(classes, zip(burst_durations_px,bandwidths_px))) #dictionnaire contenant commes cles les classes, et les valeurs sont des tuples (burst, band)
-
-def get_burst_start(classe, filename, max_y, y1): #les debuts des bursts sont dans des fichiers .txt dans le dossier 5000ms
+#the bursts starts are in .txt files 
+def get_burst_start(classe, filename, max_y, y1):
     file = os.path.join(path_5000, classe, filename)
     try:
         burst_starts=open(file, 'r').read().splitlines()
-        
-    except IOError: #pour les signaux 'continus'
+    #for continuous signals    
+    except IOError: 
         return [y1]
     
     burst_starts_px=[]
     for k in range(len(burst_starts)):
         start_time = int(burst_starts[k])
-        start_time_px = y1 + np.floor(max_y*start_time/5000) #le debut n'est pas à zero à cause de la bande blanche
+        #the start is not at 0, because of the white border
+        start_time_px = y1 + np.floor(max_y*start_time/5000) 
         burst_starts_px.append(start_time_px)
         
     return burst_starts_px
     
-    
-    
-
-
